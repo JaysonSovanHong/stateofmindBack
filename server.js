790ef279647b16d99cfd23eb1b41cb6924796688
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-04-16 13:11:56 -0400

    cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
 const rowdy = require('rowdy-logger')
 const routesReport = rowdy.begin(app)
